c6c0318d59030c6bb11289ff7a2281fb8fad4fc8
KoKoLates <the21515@gmail.com>
2023-01-13 04:00:17 +0800

    update node with MVO and data log

--- keyboard_teleop/src/keyboard_teleop_node.py
+++ keyboard_teleop/src/keyboard_teleop_node.py
@@ -1,23 +1,21 @@
 #! /usr/bin/env python3
 
-import threading
-
-import rospy
+import rospy, threading
 import sys, select, termios, tty
 
-from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
 
 move_bindings = {
     'w':( 0, 0, 1, 0),  # Up
     's':( 0, 0,-1, 0),  # Down
-    'a':( 0, 0, 0, 1),  # Yaw Left
-    'd':( 0, 0, 0, -1),  # Yaw Right
+    'a':( 0, 0, 0,-1),  # Yaw Left
+    'd':( 0, 0, 0, 1),  # Yaw Right
 
     'i':( 1, 0, 0, 0),  # Front
     'k':(-1, 0, 0, 0),  # Back
-    'j':( 0, 1, 0, 0),  # Left
-    'l':( 0, -1, 0, 0),  # Right
+    'j':( 0,-1, 0, 0),  # Left
+    'l':( 0, 1, 0, 0),  # Right
 }
 
 trigger_bindings = {
@@ -44,18 +42,27 @@ class Publish_Threading(threading.Thread):
         self.start()
 
     def wait_for_subscriber(self) -> None:
+        """ Waiting for subscriber to subscribe the topics """
         i = 0
         while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
             if i == 4:
-                rospy.loginfo('Waiting for subscriber to connect to {}'.format(self.publisher.name))
+                rospy.loginfo('[INFO] Waiting for subscriber to connect to {}'.format(self.publisher.name))
                 rospy.sleep(0.5)
                 i += 1
                 i %= 5
 
         if rospy.is_shutdown():
-            rospy.logerr('Got shutdown request before subscribers connected')
+            rospy.logerr('[ERROR] Got shutdown request before subscribers connected')
 
     def update(self, x:float, y:float, z:float, theta:float, speed:float) -> None:
+        """
+        Update the command value.
+        :param x (float): x linear velocity
+        :param y (float): y linear velocity
+        :param z (float): z linear velocity
+        :param theta (float): yaw angle velocity
+        :param speed (float): the scale of each velocity
+        """
         self.condition.acquire()
         self.x, self.y, self.z, self.theta, self.speed = x, y, z, theta, speed
         self.condition.notify() # Notify publish thread that a new message obtained
@@ -85,12 +92,17 @@ class Publish_Threading(threading.Thread):
         self.publisher.publish(twist)
 
     def stop(self) -> None:
+        """ Stop the drone """
         self.flag = True
         self.update(0.0, 0.0, 0.0, 0.0, 0.0)
         self.join()
 
     @staticmethod
     def getKey(settings:list, timeout) -> str:
+        """
+        :param setting (list): system setup
+        :param timeout (float): the time out duration
+        """
         tty.setraw(sys.stdin.fileno())
         rlist, _, _ = select.select([sys.stdin], [], [], timeout)
         if rlist:
@@ -111,7 +123,7 @@ if __name__ == "__main__":
     take_off_publisher = rospy.Publisher('/tello/take_off', Empty, queue_size=1)
 
     # Get paramter sever
-    speed_scale = float(rospy.get_param('~speed', 1.0))
+    speed = float(rospy.get_param('~speed', 1.0))
     repeat = float(rospy.get_param('~repeat_rate', 0.0))
     timeout = float(rospy.get_param('~key_timeout', 0.0))
     if timeout == 0.0:
@@ -121,11 +133,10 @@ if __name__ == "__main__":
     x, y, z, theta = 0, 0, 0, 0
 
     try:
-        
         publisher_thread.wait_for_subscriber()
-        publisher_thread.update(x, y, z, theta, speed_scale)
+        publisher_thread.update(x, y, z, theta, speed)
         while True:
-            key = publisher_thread.getKey(settings, timeout)
+            key = Publish_Threading.getKey(settings, timeout)
             if key in move_bindings.keys():
                 x = move_bindings[key][0]
                 y = move_bindings[key][1]
@@ -138,17 +149,18 @@ if __name__ == "__main__":
                 twist_msgs.linear.x, twist_msgs.linear.y, twist_msgs.linear.z = 0, 0, 0
                 twist_msgs.angular.x, twist_msgs.angular.y, twist_msgs.angular.z = 0, 0, 0
 
-                if trigger_bindings[key] == -1:
+                if trigger_bindings[key] == -1: # + take off
                     publisher_thread.publisher.publish(twist_msgs)
                     take_off_publisher.publish(empty_msgs)
 
-                elif trigger_bindings[key] == -2:
+                elif trigger_bindings[key] == -2: # - land
                     publisher_thread.publisher.publish(twist_msgs)
                     land_publisher.publish(empty_msgs)
                 else:
-                    rospy.logerr('Command error: No such trigger command')
+                    rospy.logerr('[ERROR] Command error: No such trigger command')
             
             else:
+                # Skip updating if key timeout and drone already stopped.
                 if key == '' and x == 0 and y == 0 and z == 0 and theta == 0:
                     continue
 
@@ -156,8 +168,8 @@ if __name__ == "__main__":
                 if key == '\x03': # <ctrl-c> ascii for exit
                     break
             
-            publisher_thread.update(x, y, z, theta, speed_scale)
-            rospy.loginfo('x:{} y:{} z:{} theta:{}'.format(x, y, z, theta))
+            publisher_thread.update(x, y, z, theta, speed)
+            rospy.loginfo('[INFO] x:{} y:{} z:{} theta:{}'.format(x, y, z, theta))
     
     except Exception as exception:
         rospy.logerr(exception)
--- tello_node/src/tello_driver_node.py
+++ tello_node/src/tello_driver_node.py
@@ -1,24 +1,22 @@
 #! /usr/bin/env python3
-import av
-import math
-import time
-import rospy
-import threading
-import numpy as np
-import camera_info_manager as camera_info
+import numpy as np, yaml
+import av, math, time, threading, rospy
+
+from std_msgs.msg import Empty
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CompressedImage, CameraInfo
 
 from tello_node.msg import tello_status
-from tellopy._internal import tello, error, logger, event
-from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Empty
-from sensor_msgs.msg import Image, CompressedImage, CameraInfo
-from nav_msgs.msg import Odometry
+from tellopy._internal import tello, error, event
+
 
 
 class Tello_Node(tello.Tello):
     def __init__(self) -> None:
-        # Parameter Server
+        super(Tello_Node, self).__init__(port=9000)
+        """ Parameter Servers """
         self.tellop_IP = rospy.get_param('~tello_ip', '192.168.10.1')
         self.tello_cmd_server_port = int(rospy.get_param('~tello_cmd_server_port', 8889))
         self.local_cmd_client_port = int(rospy.get_param('~local_cmd_client_port', 8890))
@@ -26,25 +24,11 @@ class Tello_Node(tello.Tello):
 
         self.initial_frame = int(rospy.get_param('~initial_frame', 100))
         self.connect_time_out =rospy.get_param('~connect_time_out', 10.0)
-        self.calibration_path = rospy.get_param('~camera_calibraiton', '')
+        self.calibration_path = rospy.get_param('~camera_calibration', '')
         self.h264_encoded_stream = bool(rospy.get_param('~h264_encoded_stream', False))
 
-        # Height Information from EVENT_FLIGHT_DATA
-        # That would be more accurate then Monocular Viausl SLAM
-        self.height = 0
-
-        self.cv_bridge = CvBridge()
-        self.frame_thread = None
-        self.EVENT_VIDEO_FRAME_H264 = event.Event('video frame h264')
-
-        # Reconstruction H264 video frames
-        self.prev_seq_id = None
-        self.seq_block_count = 0
-
-        super(Tello_Node, self).__init__(port=9000)
-
-        # Tello Connection
-        rospy.loginfo('Connecting to drone {}'.format(self.tello_addr))
+        """ Connected to the drone """
+        rospy.loginfo('Connecting to drone @ {}...'.format(self.tello_addr))
         self.connect()
         try:
             self.wait_for_connection(timeout=self.connect_time_out)
@@ -53,25 +37,34 @@ class Tello_Node(tello.Tello):
             rospy.signal_shutdown(str(err))
             self.quit()
             return
-        rospy.loginfo('Connected to drone')
+        rospy.loginfo('Tello drone connection done.')
         rospy.on_shutdown(self.shutdown_callback)
 
-        # Publihser for Tello Status
+
+        """ Publisher Topics """
         self.odom_publisher = rospy.Publisher('odom', Odometry, queue_size=1, latch=True)
         self.status_publisher = rospy.Publisher('status', tello_status, queue_size=1, latch=True)
-        
-        # Publisher for image signal
+        # Image publisher
         if self.h264_encoded_stream:
             self.image_publisher_h264 = rospy.Publisher('image_raw/h264', CompressedImage, queue_size=10)
         else:
             self.image_publisher_raw = rospy.Publisher('camera/image_raw', Image, queue_size=10)
+        
+        # Camera Information
+        self.camera_info = Tello_Node.loadCalibrationFile(self.calibration_path, 'camera_front')
+        self.camera_info.header.frame_id = rospy.get_namespace() + 'camera_front'
+        self.camera_info_publisher = rospy.Publisher('/tello/camera_info', CameraInfo, queue_size=1, latch=True)
+        self.camera_info_publisher.publish(self.camera_info)
 
-        # Subscriber for control signal
+        """ Subscribe Topics"""
         rospy.Subscriber('take_off', Empty, self.take_off_callback, queue_size=1)
         rospy.Subscriber('land', Empty, self.land_callback, queue_size=1)
         rospy.Subscriber('cmd_vel', Twist, self.cmd_vel_callback, queue_size=1)
 
-        # Video Stream
+
+        """ Events Handler """
+        self.frame_thread, self.status_count = None, 0 
+        self.EVENT_VIDEO_FRAME_H264 = event.Event('video frame h264')
         if self.h264_encoded_stream:
             self.start_video()
             self.subscribe(self.EVENT_VIDEO_DATA, self.video_data_callback)
@@ -83,10 +76,16 @@ class Tello_Node(tello.Tello):
         self.subscribe(self.EVENT_LOG_DATA, self.log_data_callback)
         self.subscribe(self.EVENT_FLIGHT_DATA, self.flight_data_callback)
 
-        self.camera_info = camera_info.loadCalibrationFile(self.calibration_path, 'camera_front')
-        self.camera_info.header.frame_id = rospy.get_namespace() + 'camera_front'
-        self.camera_info_publisher = rospy.Publisher('/tello/camera_info', CameraInfo, queue_size=1,  latch=True)
-        self.camera_info_publisher.publish(self.camera_info)
+
+        """ Member fields """
+        # Height Information from EVENT_FLIGHT_DATA that obtain from IR sensor of Tello 
+        # That would be more accurate then Monocular Viausl SLAM
+        self.height = 0       
+
+        # Reconstruction H264 video frames
+        self.prev_seq_id = None
+        self.seq_block_count = 0
+
         
         rospy.loginfo('The Tello driver node is ready')
 
@@ -94,7 +93,6 @@ class Tello_Node(tello.Tello):
     def cmd_success_notify(command:str, success:bool) -> None:
         """
         Inform the user whether the command is executed or failed
-        
         :param command (str): The command to be executed expectly.
         :param success (bool): The flag that record the execution is success or not. 
         """
@@ -103,11 +101,46 @@ class Tello_Node(tello.Tello):
         else:
             rospy.loginfo('{} - failed.'.format(command))
 
-    # Callback function
+    @staticmethod
+    def loadCalibrationFile(filename: str, cname: str) -> CameraInfo:
+        """ 
+        oad calibration data from a file.
+        This function returns a `sensor_msgs/CameraInfo`_ message, based
+        on the filename parameter.  An empty or non-existent file is `not`
+        considered an error; a null CameraInfo being provided in that case.
+
+        :param filename: location of CameraInfo to read
+        :param cname: Camera name.
+        :returns: `sensor_msgs/CameraInfo`_ message containing calibration, if file readable; null calibration message otherwise.
+        :raises: :exc:`IOError` if an existing calibration file is unreadable.
+        """
+        camera_info = CameraInfo()
+        try:
+            f = open(filename)
+            calib = yaml.load(f, Loader=yaml.FullLoader)
+            if calib is not None:
+                if calib['camera_name'] != cname:
+                    rospy.logwarn("[" + cname + "] does not match name " + calib['camera_name'] + " in file " + filename)
+
+                # fill in CameraInfo fields
+                camera_info.width = calib['image_width']
+                camera_info.height = calib['image_height']
+                camera_info.distortion_model = calib['distortion_model']
+                camera_info.D = calib['distortion_coefficients']['data']
+                camera_info.K = calib['camera_matrix']['data']
+                camera_info.R = calib['rectification_matrix']['data']
+                camera_info.P = calib['projection_matrix']['data']
+
+        except IOError:
+            pass
+
+        return camera_info
+
+    """ Callback functions """
     def cmd_vel_callback(self, msg:Twist) -> None:
         self.set_pitch(msg.linear.x)
-        self.set_roll(-msg.linear.y)
-        self.set_yaw(-msg.angular.z)
+        self.set_roll(msg.linear.y)
+        self.set_yaw(msg.angular.z)
         self.set_throttle(msg.linear.z)
 
     def take_off_callback(self, msg:Empty) -> None:
@@ -125,46 +158,73 @@ class Tello_Node(tello.Tello):
             self.frame_thread.join()
 
     def flight_data_callback(self, event, sender, data, **args):
-        # THe fight data callback function is to publish the flying status of tello
-        # The content is unfinished. 
-        speed_horizontal_mps = math.sqrt(math.pow(data.north_speed, 2) + math.pow(data.east_speed, 2)) / 10.0
+        """
+        Callback function for flight data 
+        :param event: EVENT_FLIGHT_DATA
+        :param data: flight data
+        """
+        horizontal_speed = math.sqrt(math.pow(data.north_speed, 2) + math.pow(data.east_speed, 2)) / 10.0
         self.height = data.height / 10.0
         msg = tello_status(
-            height_m = data.height / 10.0,
-            fight_time_s = data.fly_time / 10.0,
-            speed_easting_mps = data.north_speed / 10.0,
-            speed_northing_mps = -data.east_speed / 10.0,
-            speed_vertival_mps = -data.ground_speed / 10.0,
-            speed_horizontal_mps = speed_horizontal_mps,
+            height = self.height,
+            # Notes that the x, y, z in Tello is different in ROS
+            # Positive x is forward, Positive y is right
+            x_speed = data.north_speed / 10.0, 
+            y_speed = data.east_speed / 10.0,
+            horizontal_speed = horizontal_speed, 
+            vertical_speed = -data.ground_speed / 10.0,
+            flight_time = data.fly_time / 10.0,
+            battery_percentage = data.battery_percentage, 
+            is_flying = data.em_sky, 
         )
         self.status_publisher.publish(msg)
 
-    def log_data_callback(self, event, sender, data, **args):
-        now = rospy.Time.now()
+        if self.status_count == 100:
+            rospy.loginfo('Battery Percentage: {}%.' .format(data.battery_percentage))
+            self.status_count = 0
+        else:
+            self.status_count += 1
 
-        # if self.tf_broadcaster:
-        #     tf = TransformStamped()
-        #     tf.header.stamp = now
-        #     tf.header.frame_id = 'base_link'
-        #     tf.child_frame_id = 'camera_link'
-        #     tf.transform.translation.x = 0.0
-        #     tf.transform.translation.y = 0.0
-        #     tf.transform.translation.z = 0.0
-        #     tf.transform.rotation.x = 0.0
-        #     tf.transform.rotation.y = 0.0
-        #     tf.transform.rotation.z = 0.0
-        #     tf.transform.rotation.w = 1.0
-        #     self.tf_broadcaster.sendTransform(tf)
+    def log_data_callback(self, event, sender, data, **args):
+        """
+        Callback function for Odometry data that record in Tello
+        This function is to transform the data into nav_msgs.msg of ROS
+        :param event: EVENT_LOG_DATA
+        :param data: Odometry data of position and velocity in three dimension
+        """
+        msg = Odometry()
+        msg.header.frame_id = rospy.get_namespace() + 'local_origin'
+        msg.header.stamp = rospy.Time.now()
+        msg.child_frame_id = rospy.get_namespace() + 'base_link'
+        
+        # The odometry sensor are IMU and Monocular Visual Odometry in tello
+        msg.pose.pose.position.z = data.mvo.pos_z
+        msg.pose.pose.position.x = data.mvo.pos_x
+        msg.pose.pose.position.y =-data.mvo.pos_y
+        msg.pose.pose.orientation.w = data.imu.q0
+        msg.pose.pose.orientation.x = data.imu.q1
+        msg.pose.pose.orientation.y = data.imu.q2
+        msg.pose.pose.orientation.z = data.imu.q3
+        # Linear speeds from MVO received in dm/sec
+        msg.twist.twist.linear.x = data.mvo.vel_y / 10
+        msg.twist.twist.linear.y = data.mvo.vel_x / 10
+        msg.twist.twist.linear.z =-data.mvo.vel_z / 10
+        msg.twist.twist.angular.x = data.imu.gyro_x
+        msg.twist.twist.angular.y = data.imu.gyro_y
+        msg.twist.twist.angular.z = data.imu.gyro_z
+        self.odom_publisher.publish(msg)
+
                 
     def video_data_callback(self, event, sender, data, **args):
-        now = time.time()
-
+        """ 
+        Callback function for video data to parse it into compression datatype
+        :param event: EVENT_VIDEO_DATA
+        :param data: video data in byte list type
+        """
         # Parsing the packet
-        seq_id = data[0]
-        sub_id = data[1]
-        packet = data[2:]
+        seq_id, sub_id, packet = data[0], data[1], data[2:]
         self.sub_last = False
-        if sub_id >= 128:
+        if sub_id >= 128: # Most Significant Bit assert 
             sub_id -= 128
             self.sub_last = True
 
@@ -175,7 +235,7 @@ class Tello_Node(tello.Tello):
                 self.seq_block_count += 1
             
             self.frame_pkts = [None] * 128
-            self.frame_time = now
+            self.frame_time = time.time()
             self.prev_seq_id = seq_id
         
         self.frame_pkts[sub_id] = packet
@@ -187,30 +247,37 @@ class Tello_Node(tello.Tello):
             else:
                 frame = b''.join(self.frame_pkts[:sub_id + 1])
 
-            self._Tello__publish(event=self.EVENT_VIDEO_FRAME_H264, data=(frame, self.seq_block_count * 256 + seq_id, self.frame_time))
+            self._Tello__publish(
+                event = self.EVENT_VIDEO_FRAME_H264, 
+                data = (frame, self.seq_block_count * 256 + seq_id, self.frame_time))
     
     def h264_video_frame_callback(self, event, sender, data, **args):
+        """
+        Callback function for h264 video to publish corresponding topic
+        :param event: EVENT_VIDEO_FRAME_H264
+        :data: pre-process video data
+        """
         frame, seq_id, frame_secs = data
         pkt_msg = CompressedImage()
         pkt_msg.header.seq = seq_id
-        pkt_msg.header.frame_id = 'tello/camera_front'
+        pkt_msg.header.frame_id = self.camera_info.header.frame_id
         pkt_msg.header.stamp = rospy.Time.from_sec(frame_secs)
         pkt_msg.data = frame
         self.image_publisher_h264.publish(pkt_msg)
 
-    # Video threading
+    """ Video Streaming function """
     def frame_work_loop(self) -> None:
-        # Skip first 300 frame to avoid initial stream lag
+        # Skip first 100 frame to avoid initial stream lag
         video_stream = self.get_video_stream()
         while self.state != self.STATE_QUIT:
             try:
                 container = av.open(video_stream)
                 break
             except BaseException as err:
-                rospy.logerr('frame grab: pyav stream failed - {}'.format(str(err)))
+                rospy.logerr('[ERROR] frame grab: pyav stream failed - {}'.format(str(err)))
                 time.sleep(1.0)
         
-        rospy.loginfo('Video stream Connected')
+        rospy.loginfo('[INFO] Video stream Connected')
         while self.state != self.STATE_QUIT:
             try:
                 for frame in container.decode(video=0):
@@ -225,22 +292,22 @@ class Tello_Node(tello.Tello):
 
                     # opencv image -> ros image message
                     try:
-                        img_msg = self.cv_bridge.cv2_to_imgmsg(img, 'rgb8')
+                        img_msg = CvBridge().cv2_to_imgmsg(img, 'rgb8')
                         img_msg.header.frame_id = rospy.get_namespace()
                     except CvBridgeError as err:
-                        rospy.logerr('frame grab: cv bridge failed - {}'.format(str(err)))
+                        rospy.logerr('[ERROR] frame grab: cv bridge failed - {}'.format(str(err)))
                         continue
 
                     self.image_publisher_raw.publish(img_msg)
                 break
 
             except BaseException as err:
-                rospy.logerr('frame grab: pyav decoder failed - {}'.format(str(err)))
+                rospy.logerr('[ERROR] frame grab: pyav decoder failed - {}'.format(str(err)))
 
-def main() -> None:
-    rospy.init_node('tello_driver_node', anonymous=False)
-    drone = Tello_Node()
-    rospy.spin()
 
 if __name__ == '__main__':
-    main()
+    rospy.init_node('tello_driver_node', anonymous=False)
+    drone = Tello_Node()
+    if drone.state != drone.STATE_QUIT:
+        rospy.spin()
+
